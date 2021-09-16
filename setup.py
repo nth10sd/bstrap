040f23db3d8e5db5879ae2fe33e8215f14ad91c7
Gary Kwong [:gkw] <nth10sd@gmail.com>
2021-09-16 14:29:50 -0700

    Bump pylint to 2.11.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ EXTRAS = {
         "isort ~= 5.9.1",
         "mypy==0.910",
         "pep8-naming ~= 0.12.1",
-        "pylint ~= 2.10.1",
+        "pylint ~= 2.11.1",
         "pytest ~= 6.2.5",
         "pytest-bandit ~= 0.6.1",
         "pytest-black ~= 0.3.12",
