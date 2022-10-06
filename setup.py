26adc7f3f53c041016974ac5db39a4e30fbec6e4
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-10-06 16:55:28 -0700

    Bump black code formatter to 22.10.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open(
 EXTRAS = {
     "test": [
         "bandit ~= 1.7.0",
-        "black ~= 22.8.0",
+        "black ~= 22.10.0",
         "coverage[toml] ~= 6.5.0",
         "flake8 ~= 4.0.1",
         "flake8-bugbear ~= 22.9.11",
