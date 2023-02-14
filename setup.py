69ac99a6364d5e42efa685041ae2c54d7e484a5b
Gary Kwong [:gkw] <nth10sd@gmail.com>
2023-02-13 17:48:44 -0800

    Update to mypy 1.0.0 and other mypy-related plugins

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ EXTRAS = {
         "flake8-typing-imports ~= 1.12.0",
         "flynt ~= 0.77",
         "isort ~= 5.11.3",
-        "mypy==0.991",
+        "mypy ~= 1.0.0",
         "pep8-naming ~= 0.13.0",
         "pylint ~= 2.16.1",
         'pyright==1.1.287; platform_system == "Linux"',
@@ -95,8 +95,8 @@ setup(
     },
     install_requires=[
         "setuptools >= 65.6.3",
-        "types-setuptools==65.6.0.3",  # Bump types-* only with mypy
-        'types-toml==0.10.8; python_version <= "3.10"',
+        "types-setuptools==67.2.0.1",  # Bump types-* only with mypy
+        'types-toml==0.10.8.3; python_version <= "3.10"',
         "wheel >= 0.38.4",
     ],
     extras_require=EXTRAS,
